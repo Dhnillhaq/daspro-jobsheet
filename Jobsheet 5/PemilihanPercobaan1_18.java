25499fa46e64d419071cbfc402adf409ed0f5f8c
Dhnillhaq <yakulhaq@gmail.com>
2023-09-25 13:13:42 +0700

    Ternary Operator modifed

--- Jobsheet 5/PemilihanPercobaan1_18.java
+++ Jobsheet 5/PemilihanPercobaan1_18.java
@@ -1,16 +1,13 @@
-import java.util.Scanner;
-public class PemilihanPercobaan1_18 {
+   import java.util.Scanner;
+   public class PemilihanPercobaan1_18 {
 
-    public static void main(String[] args) {
-         Scanner input18 = new Scanner (System.in);
-         System.out.println("Masukkan angka: ");
-         int angka = input18.nextInt();
-         if (angka % 2 == 0) {
-            System.out.println("Angka "+angka+" bilangan genap");
-         } else {
-            System.out.println("Angka "+angka+" bilangan ganjil");
-         }
-         
+      public static void main(String[] args) {
+            Scanner input18 = new Scanner (System.in);
+            System.out.println("Masukkan angka: ");
+            int angka = input18.nextInt();
+            String output = (angka % 2 == 0) ? "Angka"+angka+" bilangan genap" : "Angka"+angka+" bilangan ganjil";
+            System.out.println(output);
+           
 
-    }
-}
+      }
+   }
