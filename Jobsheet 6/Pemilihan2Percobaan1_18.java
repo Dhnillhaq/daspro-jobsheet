a3979feffdcab2f08b109f74d7a8045c3e668000
Dhnillhaq <yakulhaq@gmail.com>
2023-10-02 20:30:22 +0700

    Except Year 2100

--- Jobsheet 6/Pemilihan2Percobaan1_18.java
+++ Jobsheet 6/Pemilihan2Percobaan1_18.java
@@ -7,7 +7,7 @@ public class Pemilihan2Percobaan1_18 {
         System.out.println("Masukkan tahun");
         tahun = input18.nextInt();
 
-        if ((tahun % 4) == 0) {
+        if ((tahun % 4) == 0 && !(tahun==2100)) {
             System.out.println("Tahun Kabisat");
         } else
         System.out.println("Bukan tahun kabisat");
