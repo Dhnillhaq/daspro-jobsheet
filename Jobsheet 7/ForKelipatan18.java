5d2091078e1cd8f7a196b909ec53676b699424ac
Dhnillhaq <yakulhaq@gmail.com>
2023-10-10 20:21:43 +0700

    w/ Rata rata

--- Jobsheet 7/ForKelipatan18.java
+++ Jobsheet 7/ForKelipatan18.java
@@ -4,7 +4,7 @@ public class ForKelipatan18 {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int kelipatan, jumlah = 0, counter = 0;
+        int kelipatan, rata = 0, jumlah = 0, counter = 0;
         System.out.print("Masukkan bilangan kelipatan (1-9): ");
         kelipatan = scan.nextInt();
         for (int i = 1; i <= 50; i++) {
@@ -13,7 +13,9 @@ public class ForKelipatan18 {
                 counter++;
             }
         }
+         rata = jumlah / counter;
         System.out.printf("Banyaknya bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
         System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
+        System.out.printf("Rata-rata seluruh bilangan dari kelipatan adalah %d", rata);
     }
 }
