01716b1f183ae004642ab65ade8379583af90ee9
Dhnillhaq <yakulhaq@gmail.com>
2023-10-12 07:42:21 +0700

    Commit New

--- Jobsheet 7/WhileGaji18.java
+++ Jobsheet 7/WhileGaji18.java
@@ -10,21 +10,25 @@ public class WhileGaji18 {
         jumlahKaryawan = scan.nextInt();
         int i = 0;
         while (i < jumlahKaryawan) {
-            System.out.println("Pilihan jabatan - Direktur, Manager, Karyawan");
+            System.out.println("Pilihan jabatan - Direktur, Manajer, Karyawan");
             System.out.print("Masukkan jabatan karyawan ke-" + (i+1) + ": ");
             jabatan = scan.next();
             System.out.print("Masukkan jumlah jam lembur: ");
             jumlahLembur = scan.nextInt();
-            i++;
+            
 
             if (jabatan.equalsIgnoreCase("direktur")) {
-                continue;
+                gajiLembur = jumlahLembur * 0;
             } else if (jabatan.equalsIgnoreCase("manajer")) {
                 gajiLembur = jumlahLembur * 100000;
             } else if (jabatan.equalsIgnoreCase("karyawan")) {
                 gajiLembur = jumlahLembur * 75000;
+            } else {
+                System.out.println("Jabatan Invalid");
+                continue;
             }
             totalGajiLembur += gajiLembur;
+            i++;
         }
         System.out.println("Total gaji lembur: " + totalGajiLembur);
     }
