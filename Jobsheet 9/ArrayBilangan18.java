860d3585d22cbfef711762bed54becfc9455f494
Dhnillhaq <yakulhaq@gmail.com>
2023-10-23 21:29:14 +0700

    Use Loop

--- Jobsheet 9/ArrayBilangan18.java
+++ Jobsheet 9/ArrayBilangan18.java
@@ -1,14 +1,10 @@
 public class ArrayBilangan18 {
 
     public static void main(String[] args) {
-        int[] bil = new int[4];
-        bil[0] = 5;
-        bil[1] = 13;
-        bil[2] = -7;
-        bil[3] = 17;
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
+        int[] bil = {5, 13, -7, 17};
+        System.out.println(bil.length);
+        for (int i = 0; i <= 4; i++) {
+            System.out.println(bil[i]);
+        }
     }
 }
