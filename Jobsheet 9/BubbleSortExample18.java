70f2551bc9d38ca3576145a9f93ce402ca14b692
Dhnillhaq <yakulhaq@gmail.com>
2023-10-24 20:31:56 +0700

    Bubble Sort

---
+++ Jobsheet 9/BubbleSortExample18.java
@@ -0,0 +1,20 @@
+public class BubbleSortExample18 {
+    public static void main(String[] args) {
+        int[] intData = {34, 18, 87, 72, 32, 54, 43};
+        int temp = 0;
+
+        for (int i=0; i < intData.length; i++) {
+            for (int j=1; j < intData.length; j++) {
+                if (intData[j-1] > intData[j]) {
+                    temp = intData[j];
+                    intData[j] = intData[j-1];
+                    intData[j-1] = temp;
+                }
+            }
+        }
+        System.out.println("Hasil pengurutan:");
+        for (int i = 0; i < intData.length; i++){
+            System.out.println(intData[i]);
+        }
+    }
+}
