84f5a7c49e307a709cf6ea6d411d0e0c61559b35
Dhnillhaq <yakulhaq@gmail.com>
2023-10-24 10:33:05 +0700

    Lulus & Tidak Lulus

--- Jobsheet 9/ArrayNilai18.java
+++ Jobsheet 9/ArrayNilai18.java
@@ -4,12 +4,16 @@ public class ArrayNilai18 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int[] nilaiAkhir = new int[10];
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nilaiAkhir.length; i++) {
             System.out.print("Masukkan nilai akhir ke-"+i+" : ");
             nilaiAkhir[i] = input.nextInt();
         }
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai akhir ke-"+i+" adalah "+nilaiAkhir[i]);
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Mahasiswa ke-"+i+" lulus!");
+            } else {
+                System.out.println("Mahasiswa ke-"+i+" mohon maaf Tidak lulus:)");
+            }
         }
     }
 }
