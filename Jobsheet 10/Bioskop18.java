44d91b130c97d7b583677c5a2fdd32db2c404a00
Dhnillhaq <yakulhaq@gmail.com>
2023-10-30 21:15:46 +0700

    Using for Each

--- Jobsheet 10/Bioskop18.java
+++ Jobsheet 10/Bioskop18.java
@@ -1,12 +1,24 @@
 public class Bioskop18 {
     public static void main(String[] args) {
-        String[][] penonton = new String[4][2];
-        for (int i = 0; i < penonton.length; i++) {
-            System.out.println("panjang baris ke-" + (i+1)+" : "+penonton[i].length);
+        String[][] penonton = new String[4][];
+        penonton[0] = new String[2];
+        penonton[1] = new String[2];
+        penonton[2] = new String[2];
+        penonton[3] = new String[2];
+        penonton [0][0] = "Amin";
+        penonton [0][1] = "Bena";
+        penonton [1][0] = "Candra";
+        penonton [1][1] = "Dela";
+        penonton [2][0] = "Eka";
+        penonton [2][1] = "Farhan";
+        penonton [3][0] = "Gisel";
+        penonton [3][1] = "Asep";
+        
+        for (String[] barisPenonton : penonton) {
+            System.out.println("Panjang baris: " + barisPenonton.length);
         }
-        System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);
-        System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]);
-        System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
-        System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println( String.join(", ", penonton[i]));
+        }
     }
 } 
