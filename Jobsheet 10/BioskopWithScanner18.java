1a26db49ded50bf6b1e97bd7c7f244691ba87af4
Dhnillhaq <yakulhaq@gmail.com>
2023-10-30 21:39:52 +0700

    w/ Scanner

---
+++ Jobsheet 10/BioskopWithScanner18.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class BioskopWithScanner18 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int baris, kolom;
+        String nama, next;
+        String[][] penonton = new String[4][2];
+        while (true) {
+            System.out.print("Masukkan nama: ");
+            nama = sc.nextLine();
+            System.out.print("Masukkan baris: ");
+            baris = sc.nextInt();
+            System.out.print("Masukkan kolom: ");
+            kolom = sc.nextInt();
+            sc.nextLine();
+            penonton[baris-1][kolom-1] = nama;
+            System.out.print("Input penonton lainnya? (y/n): ");
+            next = sc.nextLine();
+
+            if (next.equalsIgnoreCase("n")) {
+                break;
+            }
+        }
+    }
+}
