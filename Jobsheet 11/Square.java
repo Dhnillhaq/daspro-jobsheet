a1401ce3d6019186f569849451c0f6e1ca813273
Dhnillhaq <yakulhaq@gmail.com>
2023-11-07 09:45:32 +0700

    feat: Jobsheet 11

--- Jobsheet 11/Square.java
+++ Jobsheet 11/Square.java
@@ -5,7 +5,7 @@ public class Square {
         System.out.print("Masukkan nilai N = ");
         int N = sc.nextInt();
         for (int iOuter = 1; iOuter <= N; iOuter++) {
-            for (int i = 0; i <= N; i++) {
+            for (int i = 1; i <= N; i++) {
                 System.out.print("*");
             }
             System.out.println();
